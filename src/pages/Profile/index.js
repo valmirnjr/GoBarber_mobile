402943c6268b851e276648ed3ba11be765a60eec
Valmir Jr <valmir.junior@ee.ufcg.edu.br>
2020-01-30 02:43:28 -0300

    update: logout from application

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,6 +5,7 @@ import Icon from "react-native-vector-icons/MaterialIcons";
 
 import Background from "~/components/Background";
 import { updateProfileRequest } from "~/store/modules/user/actions";
+import { signOut } from "~/store/modules/auth/actions";
 
 import {
   Container,
@@ -13,6 +14,7 @@ import {
   Form,
   FormInput,
   SubmitButton,
+  LogoutButton,
 } from "./styles";
 
 export default function Profile() {
@@ -46,6 +48,10 @@ export default function Profile() {
         confirmPassword,
       })
     );
+  }
+
+  function handleLogout() {
+    dispatch(signOut());
   }
 
   return (
@@ -114,6 +120,7 @@ export default function Profile() {
           />
 
           <SubmitButton onPress={handleSubmit}>Atualizar perfil</SubmitButton>
+          <LogoutButton onPress={handleLogout}>Sair do GoBarber</LogoutButton>
         </Form>
       </Container>
     </Background>
